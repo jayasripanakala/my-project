944c7de806dace75f564e049a616931afe3f8ad0
jayasripanakala <152590850+jayasripanakala@users.noreply.github.com>
2023-12-05 13:35:56 +0530

    Add files via upload

---
+++ Array.js
@@ -0,0 +1,52 @@
+// let arr = ['h', 'e'];
+// let arr1 = arr;
+// arr1.push('l');
+
+// console.log(arr); // ["h", "e", "l"]
+// console.log(arr1); // ["h", "e", "l"]
+
+// let arr = ['h', 'e'];
+// arr.name = 'John';
+
+// console.log(arr); // ["h", "e"]
+// console.log(arr.name); // "John"
+// console.log(arr['name']); // "John"
+
+// const myArray = ['h', 'e', 'l', 'l', 'o'];
+
+// // first element
+// console.log(myArray[0]);  // "h"
+
+// // second element
+// console.log(myArray[1]); // "e"
+
+
+// empty array
+// const myList = [ ];
+
+// array of numbers
+// const numberArray = [ 2, 4, 6, 8];
+
+// array of strings
+// const stringArray = [ 'eat', 'work', 'sleep'];
+
+// array with mixed data types
+// const newData = ['work', 'exercise', 1, true];
+
+
+// const newData = [
+//     {'task1': 'exercise'},
+//     [1, 2 ,3],
+//     function hello() { console.log('hello')}
+// ];
+
+// let prime_numbers = [2, 3, 5, 7, 9, 11];
+
+// replace 1 element from index 4 by 13
+// let removedElement = prime_numbers.splice(4, 1, 13);
+// console.log(removedElement);
+// console.log(prime_numbers);
+
+let languages = ["java","python","html","CSS"]
+
+let removed = languages splice{}
